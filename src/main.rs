986c7a9ab5886c7c00d1d99c5cf7bd8eec4b5059
JinwooSeong <brianseong.dev@gmail.com>
2024-08-05 06:15:30 -0400

    fibonacci code implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,126 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt::Debug;
+use std::marker::PhantomData;
+
+use p3_air::{Air, AirBuilder, BaseAir};
+use p3_field::{AbstractField, Field};
+use p3_matrix::Matrix;
+use p3_matrix::dense::RowMajorMatrix;
+
+use p3_challenger::{HashChallenger, SerializingChallenger32};
+use p3_circle::CirclePcs;
+use p3_commit::ExtensionMmcs;
+use p3_field::extension::BinomialExtensionField;
+use p3_fri::FriConfig;
+use p3_keccak::Keccak256Hash;
+use p3_merkle_tree::FieldMerkleTreeMmcs;
+use p3_mersenne_31::Mersenne31;
+use p3_symmetric::{CompressionFunctionFromHasher, SerializingHasher32};
+use p3_uni_stark::{prove, verify, StarkConfig};
+use tracing_forest::util::LevelFilter;
+use tracing_forest::ForestLayer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{EnvFilter, Registry};
+
+pub struct FibonacciAir {
+    pub num_steps: usize,
+    pub final_value: u32,
+}
+
+impl<F: Field> BaseAir<F> for FibonacciAir {
+    fn width(&self) -> usize {
+        2 // For current and next Fibonacci number
+    }
+}
+
+impl<AB: AirBuilder> Air<AB> for FibonacciAir {
+    fn eval(&self, builder: &mut AB) {
+        let main = builder.main();
+        let local = main.row_slice(0);
+        let next = main.row_slice(1);
+
+        // Enforce starting values
+        builder.when_first_row().assert_eq(local[0], AB::Expr::zero());
+        builder.when_first_row().assert_eq(local[1], AB::Expr::one());
+
+        // Enforce state transition constraints
+        builder.when_transition().assert_eq(next[0], local[1]);
+        builder.when_transition().assert_eq(next[1], local[0] + local[1]);
+
+        // Constrain the final value
+        let final_value = AB::Expr::from_canonical_u32(self.final_value);
+        builder.when_last_row().assert_eq(local[1], final_value);
+    }
+}
+
+pub fn generate_fibonacci_trace<F: Field>(num_steps: usize) -> RowMajorMatrix<F> {
+    let mut values = Vec::with_capacity(num_steps * 2);
+    let mut a = F::zero();
+    let mut b = F::one();
+    for _ in 0..num_steps {
+        values.push(a);
+        values.push(b);
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    RowMajorMatrix::new(values, 2)
+}
+
+fn main() -> Result<(), impl Debug> {
+    let env_filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::INFO.into())
+        .from_env_lossy();
+
+    Registry::default()
+        .with(env_filter)
+        .with(ForestLayer::default())
+        .init();
+
+    type Val = Mersenne31;
+    type Challenge = BinomialExtensionField<Val, 3>;
+
+    type ByteHash = Keccak256Hash;
+    type FieldHash = SerializingHasher32<ByteHash>;
+    let byte_hash = ByteHash {};
+    let field_hash = FieldHash::new(Keccak256Hash {});
+
+    type MyCompress = CompressionFunctionFromHasher<u8, ByteHash, 2, 32>;
+    let compress = MyCompress::new(byte_hash);
+
+    type ValMmcs = FieldMerkleTreeMmcs<Val, u8, FieldHash, MyCompress, 32>;
+    let val_mmcs = ValMmcs::new(field_hash, compress);
+
+    type ChallengeMmcs = ExtensionMmcs<Val, Challenge, ValMmcs>;
+    let challenge_mmcs = ChallengeMmcs::new(val_mmcs.clone());
+
+    type Challenger = SerializingChallenger32<Val, HashChallenger<u8, ByteHash, 32>>;
+
+    let fri_config = FriConfig {
+        log_blowup: 1,
+        num_queries: 100,
+        proof_of_work_bits: 16,
+        mmcs: challenge_mmcs,
+    };
+
+    type Pcs = CirclePcs<Val, ValMmcs, ChallengeMmcs>;
+    let pcs = Pcs {
+        mmcs: val_mmcs,
+        fri_config,
+        _phantom: PhantomData,
+    };
+
+    type MyConfig = StarkConfig<Pcs, Challenge, Challenger>;
+    let config = MyConfig::new(pcs);
+
+    let num_steps = 8; // Choose the number of Fibonacci steps
+    let final_value = 21; // Choose the final Fibonacci value
+    let air = FibonacciAir { num_steps, final_value };
+    let trace = generate_fibonacci_trace::<Val>(num_steps);
+
+    let mut challenger = Challenger::from_hasher(vec![], byte_hash);
+    let proof = prove(&config, &air, &mut challenger, trace, &vec![]);
+
+    let mut challenger = Challenger::from_hasher(vec![], byte_hash);
+    verify(&config, &air, &mut challenger, &proof, &vec![])
 }
